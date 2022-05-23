f575b1dc41688cb3c980ea8e47e509afe182bf4f
pattWill <pattywilliams20@gmail.com>
2022-05-23 16:58:54 -0300

    primer commit

---
+++ my-app/src/components/App.js
@@ -0,0 +1,19 @@
+
+import '../App.css';
+import SlideBar from './SlideBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+  
+
+        <SlideBar />
+      
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
